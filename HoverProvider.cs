9b4ee2ba21c3e773e379fa11b33cbe7903de9af7
Joshua Raphael <josh.n.raphael@gmail.com>
2025-06-06 09:15:59 -0700

    detect word token at position

--- HoverProvider.cs
+++ HoverProvider.cs
@@ -3,6 +3,7 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Window;
+using System.Text;
 
 namespace RAScriptLanguageServer
 {
@@ -30,8 +31,51 @@ namespace RAScriptLanguageServer
             var buffer = _bufferManager.GetBuffer(documentPath);
             var txt = buffer.ToString();
             var line = txt.Split('\n')[lineNum];
-            _router.Window.LogInfo($"{line}");
-            return line;
+            var index = Convert.ToInt32(character);
+
+            if (index >= line.Length)
+            {
+                return "";
+            }
+            var initialChar = line[Convert.ToInt32(character)];
+            StringBuilder word = new StringBuilder();
+            if (IsWordLetter(initialChar))
+            {
+                word.Append(initialChar);
+                var hasLeft = index > 0;
+                var hasRight = index < line.Length - 1;
+                var len = line.Length;
+                if (hasLeft)
+                {
+                    for (int i = index - 1; i >= 0; i--)
+                    {
+                        if (IsWordLetter(line[i]))
+                        {
+                            word.Insert(0, line[i]); // Prepend
+                            continue;
+                        }
+                        break;
+                    }
+                }
+                if (hasRight)
+                {
+                    for (int i = index + 1; i < line.Length; i++)
+                    {
+                        if (IsWordLetter(line[i]))
+                        {
+                            word.Append(line[i]);
+                            continue;
+                        }
+                        break;
+                    }
+                }
+            }
+            return word.ToString();
+        }
+
+        private bool IsWordLetter(char c)
+        {
+            return char.IsLetterOrDigit(c) || c == '_';
         }
 
         public override Task<Hover> Handle(HoverParams request, CancellationToken cancellationToken)
@@ -40,14 +84,18 @@ namespace RAScriptLanguageServer
             var line = request.Position.Line;
             var character = request.Position.Character;
             _router.Window.LogInfo($"Opssssssssned buffer for document:");
-            var content = new List<MarkedString>
-            {
-                new MarkedString(GetWordAtPosition(documentPath, line, character))
-            };
-            var result = new Hover
-            {
-                Contents = new MarkedStringsOrMarkupContent(
-                    content.ToArray())
+            var result = new Hover();
+            var word = GetWordAtPosition(documentPath, line, character);
+            if (word.Length != 0) {
+                var content = new List<MarkedString>
+                {
+                    new MarkedString(GetWordAtPosition(documentPath, line, character))
+                };
+                result = new Hover
+                {
+                    Contents = new MarkedStringsOrMarkupContent(
+                        content.ToArray())
+                };
             };
             return Task.FromResult(result);
         }
