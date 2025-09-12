42c7da908c0095f513bf5d24829234d8c94d2b32
Joshua Raphael <josh.n.raphael@gmail.com>
2025-09-12 10:40:55 -0700

    enable hover info for 'this' keyword

--- src/HoverProvider.cs
+++ src/HoverProvider.cs
@@ -26,12 +26,38 @@ namespace RAScriptLanguageServer
             var t = request.Position;
             var buffer = _bufferManager.GetBuffer(documentPath);
             var txt = buffer?.GetDocumentText();
-            if (txt != null && txt.Length > 0)
+            if (buffer != null && txt != null && txt.Length > 0)
             {
-                var word = buffer?.GetParser().GetWordAtPosition(txt, request.Position);
-                if (word != null && word.Word.Length != 0)
+                var word = buffer.GetParser().GetWordAtPosition(txt, request.Position);
+                if (word != null && word.Word.Length != 0 && word.Start != -1 && word.End != -1)
                 {
-                    // string hoverClass = buffer?.GetParser().DetectClass()
+                    string hoverClass = buffer.GetParser().DetectClass(word.Start);
+                    int offset = word.Start - 1;
+
+                    // Special case: this keyword should show the class hover info
+                    if (word.Word == "this")
+                    {
+                        List<HoverData>? definitions = buffer.GetParser().GetHoverData(hoverClass);
+                        if (definitions != null)
+                        {
+                            foreach (HoverData hoverData in definitions)
+                            {
+                                if (hoverData.ClassName == "")
+                                {
+                                    var content = new List<MarkedString>();
+                                    foreach (var l in hoverData.Lines)
+                                    {
+                                        content.Add(new MarkedString(l));
+                                    }
+                                    Hover result = new()
+                                    {
+                                        Contents = new MarkedStringsOrMarkupContent(content)
+                                    };
+                                    return Task.FromResult<Hover?>(result);
+                                }
+                            }
+                        }
+                    }
                     // var hoverText = buffer?.GetParser().GetHoverText(word);
                     // if (hoverText != null && hoverText.Length > 0)
                     // {
